dac9b1a02bdc07be13b5266d5a80b338af79cdc2
Will G Piro <willgpiro@gmail.com>
2020-02-20 18:01:20 -0800

    scripts setup, dependencies installed

---
+++ data/seed.js
@@ -0,0 +1,11 @@
+{
+    id: SERIAL PRIMARY KEY,
+    flavor: VARCHAR(256),
+    img_url: VARCHAR(256),
+    type: VARCHAR(256),
+    dairy_free: BOOLEAN,
+    vegan: BOOLEAN,
+    will_licks: INTEGER,
+    logan_licks: INTEGER
+}
+
